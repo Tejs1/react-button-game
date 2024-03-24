9bd6a615af2d14a0764eff1b3c4222b1064af735
Tejas Thorat <tejsrelax@gmail.com>
2024-03-24 08:45:02 +0530

    Add keyboard controls and button styling to game page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { ReactEventHandler, useEffect, useState } from "react"
 import Image from "next/image"
 import {
 	ChevronRightIcon,
@@ -7,20 +8,70 @@ import {
 	ChevronDownIcon,
 } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
+import { Dir } from "fs"
+const directions = ["up", "left", "down", , "right"] as const
+type Direction = (typeof directions)[number]
 
 export default function Home() {
+	const [activeButton, setActiveButton] = useState("")
+
+	useEffect(() => {
+		const handleKeyDown = e => {
+			switch (e.key) {
+				case "w":
+				case "ArrowUp":
+					setActiveButton("up")
+					handleActions("up")
+					break
+				case "s":
+				case "ArrowDown":
+					setActiveButton("down")
+					handleActions("down")
+					break
+				case "a":
+				case "ArrowLeft":
+					setActiveButton("left")
+					handleActions("left")
+					break
+				case "d":
+				case "ArrowRight":
+					setActiveButton("right")
+					handleActions("right")
+					break
+			}
+		}
+
+		const handleKeyUp = () => {
+			setActiveButton("")
+		}
+
+		window.addEventListener("keydown", handleKeyDown)
+		window.addEventListener("keyup", handleKeyUp)
+
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown)
+			window.removeEventListener("keyup", handleKeyUp)
+		}
+	}, [])
+
+	const handleActions = (direction: Direction) => {
+		console.log(`Move ${direction}`)
+	}
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="game">
 				<div className="game-board">
-					{/* <Board /> */}
 					<div className="board"></div>
-					{/* <Controller /> */}
 					<div className="grid grid-rows-2 grid-cols-3 gap-2">
-						<ButtonIcon direction="up" className="col-start-2 col-end-3 " />
-						<ButtonIcon direction="left" className="col-start-1 col-end-2 " />
-						<ButtonIcon direction="down" className="col-start-2 col-end-3 " />
-						<ButtonIcon direction="right" className="col-start-3 col-end-4 " />
+						{directions.map(dir => (
+							<ButtonIcon
+								key={dir}
+								direction={dir}
+								active={activeButton === dir}
+								handleActions={handleActions}
+							/>
+						))}
 					</div>
 					<div className="controller">
 						<button>Start</button>
@@ -29,8 +80,8 @@ export default function Home() {
 					</div>
 				</div>
 				<div className="game-info">
-					<div>{/* status */}</div>
-					<ol>{/* TODO */}</ol>
+					<div></div>
+					<ol></ol>
 				</div>
 			</div>
 		</main>
@@ -39,34 +90,41 @@ export default function Home() {
 
 export function ButtonIcon({
 	direction,
+	active,
+	handleActions,
 	className,
 }: {
-	direction: "up" | "down" | "left" | "right"
+	direction: Direction
+	active?: boolean
+	handleActions: Function
 	className?: string
 }) {
 	let Icon = ChevronRightIcon
+	let position = ""
 	switch (direction) {
 		case "up":
 			Icon = ChevronUpIcon
+			position = "col-start-2 col-end-3 "
 			break
 		case "down":
 			Icon = ChevronDownIcon
+			position = "col-start-2 col-end-3 "
 			break
 		case "left":
 			Icon = ChevronLeftIcon
+			position = "col-start-1 col-end-2 "
 			break
 		case "right":
 			Icon = ChevronRightIcon
+			position = "col-start-3 col-end-4 "
 			break
 	}
-	const handleActions = (direction: string) => {
-		console.log(`Move ${direction}`)
-	}
+
 	return (
 		<Button
 			variant="outline"
 			size="icon"
-			className={className}
+			className={`${position} ${active ? "bg-blue-500" : ""} ${className}`}
 			onClick={() => handleActions(direction)}
 		>
 			<Icon className="h-4 w-4" />
